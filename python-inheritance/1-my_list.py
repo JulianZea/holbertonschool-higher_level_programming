eea50fbe4b626e6d4d3a405696e6df5743923523
JulianZea <cesarjulianzeabravo@gmail.com>
2022-09-28 21:50:02 -0500

    that inherits from list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""My list"""
+
+
+class MyList(list):
+    """Class MyList that inherits from list"""
+    def print_sorted(self):
+        """
+        Print list sorted
+        """
+        new_list = self[:]
+        print(sorted(new_list))
