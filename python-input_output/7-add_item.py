d0b50eadd665d1eb448809d4b7943b2c2050b8e6
JulianZea <cesarjulianzeabravo@gmail.com>
2022-10-02 16:18:20 -0500

    fix file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
+"""Load, add save"""
 import json
 import sys
-import os.path
+import os
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-if os.path.isfile(filename):
-    obj = load_from_json_file(filename)
-else:
-    obj = []
-obj.extend(sys.argv[1:])
-save_to_json_file(obj, filename)
+"""Write a script that adds all arguments to a Python list, and then save them to a file"""
+filename = 'add_item.json'
+with open(filename, 'a+') as f:
+    pass
+
+try:
+    newlist = load_from_json_file(filename)
+except ValueError:
+    newlist = []
+l = sys.argv[1:]
+newlist += l
+save_to_json_file(newlist, filename)
