966043b81ac3ed307fa3897b40d40ae20352b4f6
JulianZea <cesarjulianzeabravo@gmail.com>
2022-10-02 23:20:49 -0500

    Write a class Student that defines a student

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,30 +1,47 @@
 #!/usr/bin/python3
-"""Student to JSON"""
+"""Module 13-student.
+Creates a Student class.
+"""
 
 
 class Student:
-    """class student"""
+    """Class that defines a student.
+    Public attributes:
+        - first_name
+        - last_name
+        - age
+    Public method to_json().
+    Public method reload_from_json().
+    """
 
     def __init__(self, first_name, last_name, age):
-        """Constructor"""
+        """Initializes the Student instance."""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of a Student instance"""
-        if type(attrs) is not list:
-            return self.__dict__
-        dict = {}
+        """Retrieves a dictionary representation
+        of a Student instance.
+        Args:
+            - attrs: list of attributes
+        Returns: the dict representation of the instance.
+        """
 
-        for i in attrs:
-            v = getattr(self, i, None)
-            if v is not None:
-                dict[i] = v
-        return dict
+        my_dict = dict()
+        if attrs and all(isinstance(x, str) for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__
 
     def reload_from_json(self, json):
-        """replaces all attributes of the Student instance"""
-        if not json:
-            return
-        self.__dic__ = json
+        """Replaces all attributes of the Student instance.
+        Args:
+            - json: dictionnary of attributes
+        """
+
+        for x in json:
+            self.__dict__.update({x: json[x]})
