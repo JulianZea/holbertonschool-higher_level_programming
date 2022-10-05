c8082b790a266cb542ba4bff579672bb32bbfd9f
JulianZea <cesarjulianzeabravo@gmail.com>
2022-10-05 12:54:04 -0500

    Update the class Rectangle by adding validation of all setter methods and instantiation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,9 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError('y must be >= 0')
         self.__y = value
+
+    def area(self):
+        """
+        returns the area value of the Rectangle instance
+        """
+        return self.height * self. width
