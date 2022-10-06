58c68c8ae5eb7722c82053a4ca948d182be709ab
JulianZea <cesarjulianzeabravo@gmail.com>
2022-10-06 17:54:47 -0500

    method update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -123,12 +123,12 @@ class Rectangle(Base):
         assigns an argument to each attribute
         """
         if len(args) >= 1:
-            self.id = *[0]
+            self.id = args[0]
         if len(args) >= 2:
-            self.width = *[1]
+            self.width = args[1]
         if len(args) >= 3:
-            self.height = *[2]
+            self.height = args[2]
         if len(args) >= 4:
-            self.x = *[3]
+            self.x = args[3]
         if len(args) >= 5:
-            self.y = *[4]
+            self.y = args[4]
