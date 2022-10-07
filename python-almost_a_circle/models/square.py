1e79934c3ea4c10f60537612495554ce8cdc9142
JulianZea <cesarjulianzeabravo@gmail.com>
2022-10-06 22:32:40 -0500

    square getter and sette

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,6 +3,7 @@
 The square
 """
 from models.rectangle import Rectangle
+from models.base import Base
 
 
 class Square(Rectangle):
@@ -23,3 +24,23 @@ class Square(Rectangle):
         """
         return('[Square] ({}) {}/{} - {}'
                .format(self.id, self.x, self.y, self.__width))
+
+    @property
+    def size(self):
+        """
+        private instance size
+        """
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """
+        private instance
+        """
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        else:
+            self.__width = value
+            self.__height = value
