58efd12b3c027bab3b93c133ccf9fb0b35f3b706
JulianZea <cesarjulianzeabravo@gmail.com>
2022-10-07 20:57:50 -0500

    class method create

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
+"""Module base.
+Defines a Base class for other classes in the project.
 """
-Base class
-"""
+
 import json
-import turtle
 
 
 class Base:
+    """Class with:
+    Private class attribute: __nb_objects
     """
-    First class
-    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """class constructor"""
-        self.id = id
+        """Initialization of a Base instance.
+        Args:
+            - id: id of the instance
+        """
 
-        if id:
+        if type(id) != int and id is not None:
+            raise TypeError("id must be an integer")
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
@@ -24,43 +29,55 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """Returns a JSON representation of list_dictionaries.
+        Args:
+            - list_dictionaries: list of dicts
+        Returns: JSON representation of the list
         """
-        sharing data representation
-        """
-        if list_dictionaries:
-            dict = json.dumps(list_dictionaries)
-            return(dict)
-        return('[]')
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        if (type(list_dictionaries) != list or
+           not all(type(x) == dict for x in list_dictionaries)):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of
+        list_objs to a file.
+        Args:
+            - list_objs: list of instances who inherits of Base
         """
-        writes the JSON string representation of list_objs to a file
         """
-        new = []
-        open_name = cls.__name__ + '.json'
-        if list_objs is not None:
-            for obj in list_objs:
-                new.append(obj.to_dictionary())
-
-        with open(open_name, 'w') as file:
-            file.write(cls.to_json_string(new))
+        if type(list_objs) != list and list_objs is not None:
+            raise TypeError("list_objs must be a list of instances")
+        if any(issubclass(type(x), Base) is False for x in list_objs):
+            raise TypeError("list_objs must be a list of instances")
+        """
+        if list_objs is None or list_objs == []:
+            jstr = "[]"
+        else:
+            jstr = cls.to_json_string([o.to_dictionary() for o in list_objs])
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w') as f:
+                f.write(jstr)
 
     @staticmethod
     def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string.
+        Args:
+            - json_string: string to convert to list
         """
-        returns the list of the JSON string representation json_string
-        """
-        if json_string:
-            list = json.loads(json_string)
-            return list
-        elif json_sring is not None:
-            list = json.loads(json_string)
-            return list
-        else:
-            return('[]')
 
-	@classmethod
+        l = []
+        if json_string is not None and json_string != '':
+            if type(json_string) != str:
+                raise TypeError("json_string must be a string")
+            l = json.loads(json_string)
+        return l
+
+    @classmethod
     def create(cls, **dictionary):
         """Returns an instance with all attributes already set.
         Args:
