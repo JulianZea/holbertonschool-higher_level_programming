34fe8054199b4be6bb1ab66db42fc42e073f92d1
JulianZea <cesarjulianzeabravo@gmail.com>
2022-10-09 18:44:30 -0500

    class base id negative

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -16,7 +16,18 @@ class Test_Base(unittest.TestCase):
 		self.assertEqual(b1.id, 15)
 		self.assertEqual(b2.id, 20)
 		self.assertEqual(b3.id, 4)
+
 	def test_Base_empty(self):
 		"""Method id empty"""
 		b1 = Base()
 		self.assertEqual(b1.id, 1)
+
+	def test_Base_Negative(self):
+		"""Method id negative"""
+		b1 = Base(-15)
+		b2 = Base(-20)
+		b3 = Base(-4)
+
+		self.assertEqual(b1.id, -15)
+		self.assertEqual(b2.id, -20)
+		self.assertEqual(b3.id, -4)
