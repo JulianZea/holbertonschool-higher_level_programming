82d5928e852d75821da23bdb4f478f146caf5f3a
JulianZea <cesarjulianzeabravo@gmail.com>
2022-10-09 20:50:02 -0500

    class empty and class exist

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python2
-
 """Unittest for class Base()"""
 
 import unittest
@@ -43,13 +42,22 @@ class Test_Base(unittest.TestCase):
 		json_dictionary = Base.to_json_string([])
 		self.assertEqual(json_dictionary, '[]')
 
-	def test_Base_From_json_string_exist(self):
+	def test_Base_to_json_string_exist(self):
 		"""Methods validates a parameter exist"""
 		dictionary = {'x': 3, 'width': 16, 'id': 1, 'height': 9, 'y': 10}
 		json_dictionary = Base.to_json_string([dictionary])
 		self.assertEqual(json_dictionary, '[{"x": 3, "width": 16, "id": 1, "height": 9, "y": 10}]')
 
-	def test_Base_From_json_string_None(self):
-		"""methods From json_string validates a parameter None"""
+	def test_Base_from_json_string_None(self):
+		"""methods from_json_string validates a parameter None"""
 		list_output = Rectangle.from_json_string(None)
 		self.assertEqual(list_output, [])
+
+	def test_Base_from_json_string_Empty(self):
+		"""This methods from_json_string validates a parameter Empty"""
+		list_output = Rectangle.from_json_string('[]')
+		self.assertEqual(list_output, [])
+	def test_Base_from_json_string_exist(self):
+		"""method from_json_string validates a parameter Exist"""
+		list_output = Rectangle.from_json_string('[{"height": 5, "width": 11, "id": 90}, {"height": 8, "width": 2, "id": 9}]')
+		self.assertEqual(list_output, [{'height': 5, 'width': 11, 'id': 90}, {'height': 8, 'width': 2, 'id': 9}])
