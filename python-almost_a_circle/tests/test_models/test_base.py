d00ff355fc061da2efb1c1bf71dd534a7aac0916
JulianZea <cesarjulianzeabravo@gmail.com>
2022-10-09 19:02:09 -0500

    class base json string none

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -31,3 +31,8 @@ class Test_Base(unittest.TestCase):
 		self.assertEqual(b1.id, -15)
 		self.assertEqual(b2.id, -20)
 		self.assertEqual(b3.id, -4)
+
+	def test_Base_to_json_string_None(self):
+		"""Method validates a parameter empty"""
+			json_dictionary = Base.to_json_string(None)
+			self.assertEqual(json_dictionary, '[]')
