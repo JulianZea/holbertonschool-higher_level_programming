3a5f5e3ede5e88f80e98fe335bc95dba5924ee4d
JulianZea <cesarjulianzeabravo@gmail.com>
2022-11-13 23:26:19 -0500

    Delete states fix file

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-"""Delete all State objects with name containing letter "a"
-    from db 'hbtn_0e_6_usa'
-    Script should take 3 args: username, pw, and db name
-    Must use SQLAlchemy
+"""deletes all State objects with a name containing the letter a from
 """
 import sys
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 from model_state import Base, State
 
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import (sessionmaker)
+
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), echo=True)
+    Base.metadata.create_all(engine)
+
+    # create a configured "Session" class
     Session = sessionmaker(bind=engine)
+
+    # create a Session
     session = Session()
 
-    for instance in session.query(State).filter(State.name.like('%a%')):
-        session.delete(instance)
-
+    # delete records
+    query = session.query(State).filter(State.name.like('%a%'))
+    for deleted_rec in query.all():
+        session.delete(deleted_rec)
     session.commit()
+
+    session.close()
