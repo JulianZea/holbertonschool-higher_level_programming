31afcac6f49d7563659a4c0fd769c4b4c77aca52
JulianZea <cesarjulianzeabravo@gmail.com>
2022-11-13 23:21:31 -0500

    fix file

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-"""Add the State object "Louisiana" to db 'hbtn_0e_6_usa'
-    Print the new 'states.id' after creation
-    Script should take 3 args: username, pw, and db name
-    Must use SQLAlchemy
+"""adds the State object “Louisiana” to the database hbtn_0e_6_usa
 """
 import sys
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 from model_state import Base, State
 
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import (sessionmaker)
+
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), echo=False)
+    Base.metadata.create_all(engine)
+
+    # create a configured "Session" class
     Session = sessionmaker(bind=engine)
+
+    # create a Session
     session = Session()
 
-    l = State(name='Louisiana')
-    session.add(l)
+    # add new record
+    new_rec = State(name="Louisiana")
+    session.add(new_rec)
     session.commit()
 
-    print(l.id)
+    print(session.query(State).count())
+
+    session.close()
