bfbbf8c3da02ae1566b7b3ad8035f62375e843bf
JulianZea <cesarjulianzeabravo@gmail.com>
2022-11-13 23:31:02 -0500

    fix file

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -14,11 +14,11 @@ if __name__ == "__main__":
                          host='localhost',
                          port=3306)
     cur = db.cursor()
-    cmd = """SELECT cities.name
+    cmd = "SELECT cities.name
          FROM states
          INNER JOIN cities ON states.id = cities.state_id
          WHERE states.name=%s
-         ORDER BY cities.id ASC"""
+         ORDER BY cities.id ASC"
     cur.execute(cmd, (sys.argv[4],))
     allCities = cur.fetchall()
 
