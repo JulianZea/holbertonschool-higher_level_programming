641c148f5afe49ac3cf58809f978292a2b635941
JulianZea <cesarjulianzeabravo@gmail.com>
2022-11-18 18:36:36 -0500

    fix file

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 """This module takes in the name of a state as an argument and lists
-   all cities of that state, using the database hbtn_0e_4_usa.
-   Created on Saturday, November 12, 2022
-   @author: DaisyG Chipana Lapa
 """
 
 if __name__ == '__main__':
