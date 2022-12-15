f18dcb606b2b19b4fd75cc048dd9a84aed18fc75
JulianZea <cesarjulianzeabravo@gmail.com>
2022-12-15 16:13:45 -0500

    updated file semistandar

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -5,9 +5,9 @@ number matches a given integer. */
 
 const request = require('request');
 
-const url = ('https://swapi-api.hbtn.io/api/films/' + process.argv[2])
+const url = ('https://swapi-api.hbtn.io/api/films/' + process.argv[2]);
 
 request.get(url, (error, res, body) => {
-	if (error) throw error;
-	console.log(JSON.parse(body).title);
+  if (error) throw error;
+  console.log(JSON.parse(body).title);
 });
