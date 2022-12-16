5680b7e9e114b7a56a808f6a87d2a47522cf69d9
JulianZea <cesarjulianzeabravo@gmail.com>
2022-12-15 19:53:13 -0500

    Myself

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,22 +1,9 @@
 #!/usr/bin/node
-const fs = require('fs'); 
+
+const fs = require('fs');
 const request = require('request'); // Import the request module to make HTTP requests
 
 const url = process.argv[2]; // The URL to request, passed as the first command line argument
-const filename = process.argv[3]; // The name of the file to write, passed as the second command line argument
+const File = process.argv[3]; // The name of the file to write, passed as the second command line argument
 
-// Make an HTTP request to the specified URL and save the response body to a variable
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    // Write the response body to a file using UTF-8 encoding
-    fs.writeFile(filename, body, 'utf8', (error) => {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log(`Response body saved to ${filename}`);
-      }
-    });
-  }
-});
+request.get(url).pipe(fs.createWriteStream(File, "utf-8"));
