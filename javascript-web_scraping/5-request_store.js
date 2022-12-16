875150f64a9ab452f6f4244bdd590a4729421265
JulianZea <cesarjulianzeabravo@gmail.com>
2022-12-15 19:58:41 -0500

    fix file 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,4 +6,4 @@ const request = require('request'); // Import the request module to make HTTP re
 const url = process.argv[2]; // The URL to request, passed as the first command line argument
 const File = process.argv[3]; // The name of the file to write, passed as the second command line argument
 
-request.get(url).pipe(fs.createWriteStream(File, "utf-8"));
+request.get(url).pipe(fs.createWriteStream(File, 'utf-8'));
